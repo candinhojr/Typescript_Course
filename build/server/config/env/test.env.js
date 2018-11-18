26a6fb6c5a25502e4b4c3cd83ba8a4c411cf1630
candinhojr <candinholuiz@gmail.com>
2018-11-18 14:45:51 -0300

    Preparando o Ambiente de Testes [parte II]

---
+++ build/server/config/env/test.env.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: 'development',
+    bd: 'typescript-api-test',
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'pgroot',
+    host: 'localhost',
+    serverPort: 3000,
+    pgPort: 5432,
+    dbURL: 'postgres://postgres:pgroot@localhost:5432/typescript-api-test',
+    secret: 'S3cr3t'
+};
